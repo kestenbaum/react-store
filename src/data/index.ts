48668da5511f641ea15e8f885bb68bd358be8a5a
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-18 16:05:40 +0100

    add data for my project

---
+++ src/data/index.ts
@@ -0,0 +1,15 @@
+export const data:any[] = [
+    {
+        id: 1,
+        to: "/",
+        title: "HOME"
+    }, {
+        id: 2,
+        to: "/store",
+        title: "STORE"
+    }, {
+        id: 3,
+        to: "/about",
+        title: "ABOUT"
+    }
+]
